68e69434f58f71bd688760316768ca8865c28697
jugraj <jugraj@192.168.1.2>
2025-06-22 19:33:05 +0530

    Fix: remove .js extension from import for Vercel build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import events from "../data/events.js";
+import events from "../data/events";
 
 const Home = () => {
   return (
